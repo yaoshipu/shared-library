5a466f602ce8b8be74f595b5507071ebc8a68ad8
Spark.Yao <shiyao@microsoft.com>
2020-07-30 11:00:40 +0800

    update title

--- portal/src/settings.js
+++ portal/src/settings.js
@@ -1,6 +1,6 @@
 module.exports = {
 
-  title: 'Vue Admin Template',
+  title: 'CSS',
 
   /**
    * @type {boolean} true | false
--- portal/src/utils/get-page-title.js
+++ portal/src/utils/get-page-title.js
@@ -1,6 +1,6 @@
 import defaultSettings from '@/settings'
 
-const title = defaultSettings.title || 'Vue Admin Template'
+const title = defaultSettings.title || 'CSS Library'
 
 export default function getPageTitle(pageTitle) {
   if (pageTitle) {
